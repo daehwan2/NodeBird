cae0b8ae66fa8893ce77d1c4b81a7b085e2bfab7
Daehwan2 <dhdh0482@naver.com>
2021-06-05 19:11:13 +0900

    06. 팔로우 팔로잉 기능 해결

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(passport.session());
 app.use('/',pageRouter);
 app.use('/auth',authRouter);
 app.use('/post',postRouter);
-app.use('user',userRouter);
+app.use('/user',userRouter);
 
 app.use((req,res,next)=>{
     const error=new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
